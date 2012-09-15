0235d700533f3f9a33dbbcd72c7c5b1adc667094
a-t <aki.appdev@gmail.com>
2012-09-16 02:01:46 +0900

    Update TwitterBot.php

--- TwitterBot.php
+++ TwitterBot.php
@@ -27,5 +27,17 @@ class TwitterBot
             }
         }
     }
+
+    public function autoUnfollow()
+    {
+        $followers = $this->oauth->get('followers/ids', array('cursor' => -1));
+        $friends = $this->oauth->get('friends/ids', array('cursor' => -1));
+
+        foreach ($friends->ids as $i => $id) {
+            if (empty($followers->ids) or !in_array($id, $followers->ids)) {
+                $this->oauth->post('friendships/destroy', array('user_id' => $id));
+            }
+        }
+    }
 }
 
