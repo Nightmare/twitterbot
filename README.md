afb07eeb976df59c94d002c3ad9843bdae334451
Nightmare <la_fabio@hotmail.com>
2012-12-22 17:13:49 +0100

    added new features, see readme

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-twitterbot
+twitterbot (fork)
 ==========
 
 A simple twitter bot in PHP
+
+New Features
+------------
+* auto-follow given users ids
+* get friends list
+* get follower list
+
 
 Features
 --------
@@ -10,26 +17,40 @@ Features
 * auto-unfollow
 * search
 
+
 Usage
 -----
+    require_once('TwitterBot.php');
 
-    $key = 'YourConsumerKey';
-    $secret = 'YourConsumerSecret';
-    $token = 'YourAccessToken';
+    $key          = 'YourConsumerKey';
+    $secret       = 'YourConsumerSecret';
+    $token        = 'YourAccessToken';
     $token_secret = 'YourAccessTokenSecret';
-    
+
     $bot = new TwitterBot($key, $secret, $token, $token_secret);
     $bot->post("Hello, world!");
     $bot->autoFollow();
     $bot->autoUnfollow();
-    
+
     $query = array(
-      "q" => "github",
-      "count" => 10,
+      "q"           => "twitterbot",
+      "count"       => 10,
       "result_type" => "recent",
     );
-    
+
+    * if you want follow users from search results
     $results = $bot->search($query);
     foreach ($results->statuses as $result) {
-      echo $result->text . "¥n":
+      echo $result->text . "\n";
     }
+
+    * if you want follow users from search results
+    foreach ($results->statuses as $result) {
+      $users_id[] = $result->user->id;
+    }
+
+    $users_id = array_unique($users_id);
+
+    $new_followings = $bot->autoFollow($users_id);
+
+    "Got ". $new_followings ." new following(s)";
--- TwitterBot.php
+++ TwitterBot.php
@@ -1,53 +1,89 @@
 <?php
 
-require_once __DIR__ . '/twitteroauth/twitteroauth.php';
+require_once('twitteroauth/twitteroauth.php');
 
 class TwitterBot
 {
-    protected $oauth;
+	protected $oauth;
 
-    public function __construct($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret)
-    {
-        $this->oauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-    }
+	public function __construct($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret)
+	{
+		$this->oauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+	}
 
-    public function post($message)
-    {
-        $this->oauth->post('statuses/update', array('status' => $message));
-    }
+	public function post($message)
+	{
+		$this->oauth->post('statuses/update', array('status' => $message));
+	}
 
-    /**
-     * Follow all users who are following you.
-     */
-    public function autoFollow()
-    {
-        $followers = $this->oauth->get('followers/ids', array('cursor' => -1));
-        $friends = $this->oauth->get('friends/ids', array('cursor' => -1));
+	/**
+	 * Follow all users who are following you or follow new determined users
+	 *
+	 * $users_id: Array of users id to follow
+	 *
+	 */
+	public function autoFollow($users_id = array())
+	{
+		$new_following = 0;
+		if (empty($users_id)) {
+			$followers = $this->oauth->get('followers/ids', array('cursor' => -1));
+			$friends = $this->oauth->get('friends/ids', array('cursor' => -1));
 
-        foreach ($followers->ids as $i => $id) {
-            if (empty($friends->ids) or !in_array($id, $friends->ids)) {
-                $this->oauth->post('friendships/create', array('user_id' => $id));
-            }
-        }
-    }
+			foreach ($followers->ids as $i => $id) {
+				if (empty($friends->ids) or !in_array($id, $friends->ids)) {
+					$this->oauth->post('friendships/create', array('user_id' => $id));
+					$new_following++;
+				}
+			}
+		} else {
+			$friends = $this->oauth->get('friends/ids', array('cursor' => -1));
+			foreach ($users_id as $user_id) {
+				if (!in_array($user_id, $friends->ids)) {
+					sleep(10);
+					$this->oauth->post('friendships/create', array('user_id' => $user_id));
+					$new_following++;
+				}
+			}
+		}
 
-    /**
-     * Unfollow all users who are not following you.
-     */
-    public function autoUnfollow()
-    {
-        $followers = $this->oauth->get('followers/ids', array('cursor' => -1));
-        $friends = $this->oauth->get('friends/ids', array('cursor' => -1));
+		return $new_following;
+	}
 
-        foreach ($friends->ids as $i => $id) {
-            if (empty($followers->ids) or !in_array($id, $followers->ids)) {
-                $this->oauth->post('friendships/destroy', array('user_id' => $id));
-            }
-        }
-    }
+	/**
+	 * Unfollow all users who are not following you.
+	 */
+	public function autoUnfollow()
+	{
+		$followers = $this->oauth->get('followers/ids', array('cursor' => -1));
+		$friends = $this->oauth->get('friends/ids', array('cursor' => -1));
 
-    public function search(array $query)
-    {
-        return $this->oauth->get('search/tweets', $query);
-    }
+		foreach ($friends->ids as $i => $id) {
+			if (empty($followers->ids) or !in_array($id, $followers->ids)) {
+				$this->oauth->post('friendships/destroy', array('user_id' => $id));
+			}
+		}
+	}
+
+	public function search(array $query)
+	{
+		return $this->oauth->get('search/tweets', $query);
+	}
+
+	/**
+	 * Get followers list
+	 *
+	 */
+	public function getFollowers()
+	{
+		return $followers = $this->oauth->get('followers/ids', array('cursor' => -1));
+	}
+
+	/**
+	 * Get friends list (users you follow)
+	 *
+	 */
+	public function getFriends()
+	{
+		return $friends = $this->oauth->get('friends/ids', array('cursor' => -1));
+	}
 }
